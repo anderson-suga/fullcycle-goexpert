07c8e5f1d03247294aec89c729af10dbbb5ce137
Anderson Suga <anderson.suga@gmail.com>
2025-08-15 20:19:35 -0300

    002 - Declaração e atribuição

---
+++ 002 - Fundação/002/main.go
@@ -0,0 +1,17 @@
+package main
+
+const a = "Hello World"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Anderson"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+	a = "fd"
+
+	println(a)
+}
